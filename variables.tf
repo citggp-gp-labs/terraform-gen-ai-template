b6bc08abf9bd5a65995157150ec1e715bafa9d08
CIANDT\jmagioli <j.magioli@ext.ppay.me>
2024-06-24 16:59:12 -0300

    update variables

--- variables.tf
+++ variables.tf
@@ -1,11 +1,9 @@
 variable "project_id" {
   description = "value of project name"
-  default = "gcp-warmup-enjoei"
   type = string
 }
 
 variable "location" {
   description = "value of project region"
-  default = "us-east4"
   type = string
 }
